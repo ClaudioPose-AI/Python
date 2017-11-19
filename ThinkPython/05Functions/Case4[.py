f05c03c3c012a85f4c4c8ff924eac780bc6c7916
Claudio Pose <32174901+ClaudioPose-AI@users.noreply.github.com>
2017-11-19 13:36:42 -0200

    0.0001

---
+++ ThinkPython/05Functions/Case4[.py
@@ -0,0 +1,22 @@
+import turtle
+
+def desenhaRetangulo(t, w, h):
+    """Faca a tartaruga t desenhar um retangulo de largura w e altura h."""
+    for i in range(2):
+        t.forward(w)
+        t.left(90)
+        t.forward(h)
+        t.left(90)
+
+def desenhaQuadrado(tx, tam):        # uma nova versao de desenhaQuadrado
+    desenhaRetangulo(tx, tam, tam)
+
+wn = turtle.Screen()             # Inicializa a janela
+wn.bgcolor("lightgreen")
+
+tess = turtle.Turtle()           # cria tess
+
+desenhaQuadrado(tess, 50)
+
+wn.exitonclick()
+
