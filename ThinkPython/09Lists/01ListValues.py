3da8fb224d10f280b61c3a39ca35d3dd796c2d20
Claudio Pose <32174901+ClaudioPose-AI@users.noreply.github.com>
2017-11-19 03:36:35 -0200

    01-Test

---
+++ ThinkPython/09Lists/01ListValues.py
@@ -0,0 +1,17 @@
+## Valores em uma lista
+
+vocabulario = ["iteracao", "selecao", "controle"]
+numeros = [17, 123]
+vazia = []
+lista_mista = ["ola", 2.0, 5*2, [10, 20]]
+
+print(numeros)
+print(lista_mista)
+nova_lista = [numeros, vocabulario]
+print(nova_lista)
+
+## Comprimento de uma lista
+
+uma_lista =  ["ola", 2.0, 5, [10, 20]]
+print(len(uma_lista))
+print(len(['spam!', 1, ['Brie', 'Roquefort', 'Pol le Veq'], [1, 2, 3]]))
