3074f8390bfee73676f6906384490dacee2b42d3
Claudio Pose <32174901+ClaudioPose-AI@users.noreply.github.com>
2017-11-20 01:29:02 -0200

    Test

---
+++ AulasPythonIgnZero/010 - Comparações Múltiplas/CaixaEletronico.py
@@ -0,0 +1,16 @@
+#Caixa eletronico
+
+saque = int(input('Qual o valor do saque?'))
+notas = [[100,0],[50,0],[10,0],[5,0],[1,0]]
+impressao = ''
+
+
+
+"""
+def defineNotas(valorSaque, notas):
+    for row in range(len(notas)+1):
+        if valorSaque // notas[row][0]:
+            return notas[row][1] = valorSaque / notas[row][0]
+
+print(notas)
+"""
